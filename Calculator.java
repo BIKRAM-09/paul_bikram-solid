e585105d79244cee064829b4527d014db7960ea9
BIKRAM-09 <60188096+BIKRAM-09@users.noreply.github.com>
2020-02-09 11:47:21 -0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator
+{
+public static void main(double a,double b)
+{
+    double s=a+b;
+    double c=a-b;
+    double m=a*b;
+    double d=a/b;
+    System.out.println("THE SUM OF THE GIVEN NUMBERS IS "+s);
+    System.out.println("THE SUBTRACTION OF THE GIVEN NUMBERS IS "+c);
+    System.out.println("THE PRODUCT OF THE GIVEN NUMBERS IS "+m);
+    System.out.println("THE DIVISION OF THE GIVEN NUMBERS IS "+d);
+}
+}
+
